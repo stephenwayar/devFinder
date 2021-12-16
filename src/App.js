8ad78e29f82e2a3af44bdbdf671aae8dd0c9cfcb
stephenwayar <stephenbuluswayar@gmail.com>
2021-12-16 08:36:39 +0100

    CRA and tailwind configs completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-red-500 font-bold">
+      <h1>Happy hacking!</h1>
+    </div>
+  );
+}
+export default App;
